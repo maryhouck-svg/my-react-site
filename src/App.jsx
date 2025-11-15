4e67168e7b336ccc905e8774984a95539ba8c61b
maryhouck-svg <marypresleyhouck@gmail.com>
2025-11-15 11:02:42 -0500

    Initial React project setup with Vite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+
+function App() {
+  return(
+    <div>
+      <Header />
+      <main> Financial Budgeting for Paw Points </main>
+    </div>
+  )
+}
+
+export default App
